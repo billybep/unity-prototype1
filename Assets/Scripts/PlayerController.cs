79fb9b33528066dffc023168a22223a0c25e0cd1
billybep <billy.pesoth@gmail.com>
2022-11-14 11:12:10 +0800

    add force instead of transform on vehicle, add speedometer

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,19 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody playerRb;
+    [SerializeField] TextMeshProUGUI speedometerText;
+    [SerializeField] TextMeshProUGUI rpmText;
+    public List<WheelCollider> allWheels;
+
     // Private variables
-    [SerializeField] private float speed = 20.0f;
+    [SerializeField] private float speed;
+    [SerializeField] private float horsePower = 0;
     [SerializeField] private float turnSpeed = 25.0f;
     [SerializeField] private float horizontalInput;
     [SerializeField] private float verticalInput;
+    [SerializeField] private float rpm;
 
+    
     // Public variables
     public Camera mainCamera;
     public Camera hoodCamera;
     public KeyCode switchKey;
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();    
+    }
 
     // Update is called once per frame
     // FixedUpdate ==> Call before update call
@@ -24,8 +38,16 @@ public class PlayerController : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
 
+        speed = Mathf.Round(playerRb.velocity.magnitude * 2.237f); // for KPH
+        speedometerText.SetText("Speed: " + speed + "mph");
+
+        rpm = (speed % 30) * 40;
+        rpmText.SetText("RPM: " + rpm);
+
         // Move player/vehicle based on vertical input
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        //transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+
+        playerRb.AddRelativeForce(Vector3.forward * horsePower * verticalInput);
 
         // Rotates the car based on horizontal input
         transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
